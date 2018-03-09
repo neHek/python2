7016847c3843d8a2f138d5585981f50738858961
Кокарев Иван <kokarev97@inbox.ru>
2018-03-09 17:44:28 +0300

    Добавить Ивана

--- mySite/main/admin.py
+++ mySite/main/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from main.models import Speakers
 from main.models import Section
 
-admin.site.register(Speakers)
 admin.site.register(Section)
+
+def make_ivan(modeladmin, request, queryset):
+    queryset.update(name='Иван')
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    actions = [make_ivan]
+
+admin.site.register(Speakers, SpeakerAdmin)
